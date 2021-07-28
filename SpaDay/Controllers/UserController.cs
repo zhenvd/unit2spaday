36b32d5e3fdf389d8060412e849402269053ded9
Vincent Zhen <Vincent.D.Zhen@Outlook.com>
2021-07-28 16:42:50 -0400

    first commit

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+using SpaDay.ViewModel;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index(User newUser)
+        {
+            return View(newUser); 
+        }
+
+        public IActionResult Add()
+        {
+            AddUserViewModel addUserViewModel = new AddUserViewModel();
+
+            return View(addUserViewModel);
+        }
+
+        [HttpPost]
+        [Route("/user/add")]
+        public IActionResult SubmitAddUserForm(AddUserViewModel addUserViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                User newUser = new User
+                {
+                    Username = addUserViewModel.Username,
+                    Password = addUserViewModel.Password,
+                    Email = addUserViewModel.ContactEmail
+
+                };
+                //ViewBag.user = addUserViewModel;
+                return View("Index", newUser);
+            }
+            else
+            {
+                //ViewBag.error = "Passwords do not match! Try again!";
+                //ViewBag.userName = addUserViewModel.Username;
+                //ViewBag.email = addUserViewModel.ContactEmail;
+                return View("Add", addUserViewModel);
+            }
+        }
+
+    }
+}
---
+++ SpaDay/ViewModel/AddUserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.ViewModel
+{
+    public class AddUserViewModel
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Name must be between 5 and 15 characters")]
+        public string Username { get; set; }
+
+        [Compare("VerifyPassword", ErrorMessage ="Passwords must match.")]
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Password is too long!")]
+        public string Password { get; set; }
+
+        [EmailAddress]
+        public string ContactEmail { get; set; }
+               
+        public string VerifyPassword { get; set; }
+    }
+}
